0c8d1c821e460368dc574c6d0f008bd8124d5b8d
younglust <younglust@gmail.com>
2019-04-13 16:15:19 +0200

    Fix issues

--- js/main.js
+++ js/main.js
@@ -1,67 +1,47 @@
-'use strict';
-console.log('hola');
+"use strict";
 
-//Juego de adivinar número random. Da pistas si el número es algo o bajo
-
-
-//Obtener el input en sí
-//Obtener el botón
-//Obtener valor del párrafo
-//Obtener el elemento que será el contador
-    //Cada vez que se haga click el contador suma 1
-//Escuchar click del botón
-//Añadir listener al botón
-//Generar número aleatorio y mostrarlo por consola
-    //Si el número es mayor del value mostrar mensaje: número más bajo
-    //Si número es menor del value mostrar mensaje: número más alto
-    //Si acierta mostrar mensaje: Has  ganado...
-
-
-
-
-const inputMain = document.querySelector('.main__input');
-const btnMain = document.querySelector('.main__button');
-const parrafMain = document.querySelector('.main__parraf');
-const counter = document.querySelector('.main__contador');
+const inputMain = document.querySelector(".main__input");
+const btnMain = document.querySelector(".main__button-try");
+const parrafMain = document.querySelector(".main__parraf");
+const counter = document.querySelector(".main__counter");
+const resetButon = document.querySelector(".main__reset-button");
+const defaultText = "Introduce un número y haz click en el botón 'Prueba'";
+const numberBig = "Demasiado alto";
+const numberLow = "Demasiado bajo";
+const numberWinner = "¡HAS GANADO, CAMPEONA!";
+let acc = 0;
 
 // Función que genera un número aleatorio hasta un máximo dado
 function getRandomNumber(max) {
-    return Math.ceil(Math.random() * max);
+  return Math.ceil(Math.random() * max);
 }
-
-let acc = 0;
-
-btnMain.addEventListener('click', handlerButton);
-
-const numberBig = 'Demasiado alto';
-const numberLow = 'Demasiado bajo';
-const numberWinner = '¡HAS GANADO, CAMPEONA!';
+const randomNumber = getRandomNumber(100);
 
 function handlerButton(event) {
-    event.preventDefault();
+  event.preventDefault();
 
-    // for (let i = 0; i < handlerButton; i++) {
-    //     acc += 1;
-    //     counter.innerHTML = acc;
-    //     console.log('counter ' , acc);        
-    // };
+  const everyValue = parseInt(inputMain.value);
 
+  if (everyValue === randomNumber) {
+    parrafMain.innerHTML = numberWinner;
+  } else if (everyValue > randomNumber) {
+    parrafMain.innerHTML = numberBig;
+  } else if (everyValue < randomNumber) {
+    parrafMain.innerHTML = numberLow;
+  }
 
-    const everyValue = inputMain.value;
+  console.log("Número aleatorio ", randomNumber);
 
-    const randomNumber = getRandomNumber(100);
-
-    if(everyValue === randomNumber) {
-        numberWinner;
-
-    } else if(everyValue > randomNumber) {
-
-        parrafMain.innerHTML = numberBig;
-        
-    } else if(everyValue < randomNumber) {
-        parrafMain.innerHTML = numberLow;
-    }
-
-    console.log('Número aleatorio ' , randomNumber);
+  //counter
+  acc += 1;
+  counter.innerHTML = acc;
 }
+btnMain.addEventListener("click", handlerButton);
 
+//add reset button
+function resetButton() {
+  inputMain.value = "";
+  parrafMain.innerHTML = defaultText;
+  counter.innerHTML = 0;
+}
+resetButon.addEventListener("click", resetButton);
