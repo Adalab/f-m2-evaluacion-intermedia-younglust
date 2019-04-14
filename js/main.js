5beccdd63b57196b363a86bb3ada147b2af3ca8f
younglust <younglust@gmail.com>
2019-04-14 17:16:27 +0200

    Fix new issues

--- js/main.js
+++ js/main.js
@@ -1,14 +1,16 @@
 "use strict";
-
+//Juego de adivinar número random. Da pistas si el número es algo o bajo
 const inputMain = document.querySelector(".main__input");
 const btnMain = document.querySelector(".main__button-try");
 const parrafMain = document.querySelector(".main__parraf");
 const counter = document.querySelector(".main__counter");
 const resetButon = document.querySelector(".main__reset-button");
-const defaultText = "Introduce un número y haz click en el botón 'Prueba'";
-const numberBig = "Demasiado alto";
-const numberLow = "Demasiado bajo";
-const numberWinner = "¡HAS GANADO, CAMPEONA!";
+const feedbackTextGame = {
+  default: "Introduce un número y haz click en el botón 'Prueba'",
+  big: "Demasiado alto",
+  low: "Demasiado bajo",
+  winner: "¡HAS GANADO, CAMPEONA!"
+};
 let acc = 0;
 
 // Función que genera un número aleatorio hasta un máximo dado
@@ -17,31 +19,40 @@ function getRandomNumber(max) {
 }
 const randomNumber = getRandomNumber(100);
 
-function handlerButton(event) {
-  event.preventDefault();
-
-  const everyValue = parseInt(inputMain.value);
-
-  if (everyValue === randomNumber) {
-    parrafMain.innerHTML = numberWinner;
-  } else if (everyValue > randomNumber) {
-    parrafMain.innerHTML = numberBig;
-  } else if (everyValue < randomNumber) {
-    parrafMain.innerHTML = numberLow;
-  }
-
-  console.log("Número aleatorio ", randomNumber);
-
-  //counter
+//counter
+function startCounter() {
   acc += 1;
   counter.innerHTML = acc;
 }
+
+//feedback
+function feedbackGame() {
+  const everyValue = parseInt(inputMain.value);
+
+  if (everyValue === randomNumber) {
+    parrafMain.innerHTML = feedbackTextGame.winner;
+
+  } else if (everyValue > randomNumber) {
+    parrafMain.innerHTML = feedbackTextGame.big;
+
+  } else if (everyValue < randomNumber) {
+    parrafMain.innerHTML = feedbackTextGame.low;
+  }
+}
+
+function handlerButton(event) {
+  event.preventDefault();
+  feedbackGame();
+  startCounter();
+  console.log("Número aleatorio ", randomNumber);  
+}
 btnMain.addEventListener("click", handlerButton);
 
-//add reset button
+//reset button
 function resetButton() {
   inputMain.value = "";
-  parrafMain.innerHTML = defaultText;
+  parrafMain.innerHTML = feedbackTextGame.default;
   counter.innerHTML = 0;
+  acc = 0;
 }
 resetButon.addEventListener("click", resetButton);
